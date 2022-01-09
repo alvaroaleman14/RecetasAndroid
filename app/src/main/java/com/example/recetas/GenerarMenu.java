57499a8f6aeca45d1fc59d3f28475bbf385d8170
alvaroaleman14 <alvaro14uma@uma.es>
2022-01-09 21:18:49 +0100

    media clase generarmenu

--- app/src/main/java/com/example/recetas/GenerarMenu.java
+++ app/src/main/java/com/example/recetas/GenerarMenu.java
@@ -2,26 +2,29 @@ package com.example.recetas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 
+import com.example.recetas.DB.Dish;
 import com.example.recetas.DB.RecipeBook;
 
 public class GenerarMenu extends AppCompatActivity {
-
+    Context context;
     CheckBox range1, range2, range3, gluten, crustacean, egg, fish, driedFruit, soy, dairy, mollusk, mustard, celery, lupine, sesame, sulfurDioxide;
     public final static String Name="Name";
-    RecipeBook recipeBook;
-   // SQLiteDatabase db = recipeBook.getReadableDatabase();
+    RecipeBook recipeBook = RecipeBook.getInstancia(context);
+   SQLiteDatabase db = recipeBook.getReadableDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generar_menu);
-        /*
+
         gluten = (CheckBox) findViewById(R.id.glutenFree);
         crustacean = (CheckBox) findViewById(R.id.crust);
         egg = (CheckBox) findViewById(R.id.huevos);
@@ -37,20 +40,39 @@ public class GenerarMenu extends AppCompatActivity {
         sulfurDioxide = (CheckBox) findViewById(R.id.diox);
         range1 = (CheckBox) findViewById(R.id.range1);
         range2 = (CheckBox) findViewById(R.id.range2);
-        range3 = (CheckBox) findViewById(R.id.range3);*/
+        range3 = (CheckBox) findViewById(R.id.range3);
     }
 
 
     public void CrearMenu(View view) {
 
-        //Faltan por pasar filtros
-        /*if (range1.isChecked()) {
-            String selection = RecipeBook.DishEntry.COLUMN_NAME_CALORIE + " = ?";
-            String[] selectionArgs = new String[]{"1500-2500"};
+        switch (view.getId()) {
+            case R.id.range1:
+                range2.setChecked(false);
+                range3.setChecked(false);
+                String selection1 = RecipeBook.DishEntry.COLUMN_NAME_CALORIE + " = ?";
+                String[] selectionArgs1 = new String[]{"1500-2500"};
+                Cursor cursor = db.rawQuery("SELECT * FROM DISH WHERE CALORIE BETWEEN 1500 and 2500 ORDER BY CALORIE", selectionArgs1);
+                break;
+
+            case R.id.range2:
+                range1.setChecked(false);
+                range3.setChecked(false);
+                String selection2 = RecipeBook.DishEntry.COLUMN_NAME_CALORIE + " = ?";
+                String[] selectionArgs2 = new String[]{"2500-3500"};
+                Cursor cursor2 = db.rawQuery("SELECT * FROM DISH WHERE CALORIE BETWEEN 2500 and 3500 ORDER BY CALORIE", selectionArgs2);
+                break;
+
+            case R.id.range3:
+                range1.setChecked(false);
+                range2.setChecked(false);
+                String selection3 = RecipeBook.DishEntry.COLUMN_NAME_CALORIE + " = ?";
+                String[] selectionArgs3 = new String[]{"3500-5000"};
+                Cursor cursor3 = db.rawQuery("SELECT * FROM DISH WHERE CALORIE BETWEEN 3500 and 5000 ORDER BY CALORIE", selectionArgs3);
+                break;
         }
 
-    }
-        Bundle b = new Bundle();*/
+        Bundle b = new Bundle();
        // b.putString(Name, "Prueba");
 
         Intent intento= new Intent(this, ListarPlatosMenu.class);
